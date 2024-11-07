d782380cbd0f500df9b8ad6da6e38c1ab5b80ad3
ercr07001 <ercr07001@E-PW05CGR0>
2024-11-07 12:30:11 +0100

    Add project files.

---
+++ Uppgift3.1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+namespace Uppgift
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hur gammal är du?");
+            int Ålder = int.Parse(Console.ReadLine());
+
+            if (Ålder == 16)
+            {
+                Console.WriteLine("Du är tillräckligt gammal för att vara med");
+            }
+            else if (Ålder == 17)
+            {
+                Console.WriteLine("Du är tillräckligt gammal för att vara med");
+            }
+            else if (Ålder == 18)
+            {
+                Console.WriteLine("Du är tillräckligt gammal för att vara med");
+            }
+            else if (Ålder == 19)
+            {
+                Console.WriteLine("Du är tillräckligt gammal för att vara med");
+            }
+            else if (Ålder < 16)
+            {
+                Console.WriteLine("Du är inte tillräckligt gammal nog för att vara med");
+            }
+            else if (Ålder > 19)
+            {
+                Console.WriteLine("Du är för gammal för att vara med");
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
